1028e5b8b5831b89c5bacea7b4336ab61c275903
Dominik Mueller <mullerdome@yahoo.de>
2014-06-21 21:39:22 +0200

    Workings on aspect ratio diversity
    Resize of the background plane, it adapts now to aspect ratio (not fully
    solved)

---
+++ Assets/Scripts/AspectRatioController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AspectRatioController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+    {
+        float aspect = camera.aspect;
+        Debug.Log("Aspect: " + aspect);
+
+        Debug.Log("Resizing background plane...");
+
+        Transform lTransform = GetComponentInChildren<Transform>();
+        Vector3 scale = new Vector3(lTransform.localScale.z * aspect, 1, lTransform.localScale.z);
+        lTransform.localScale = scale;
+	}
+	
+	
+}
