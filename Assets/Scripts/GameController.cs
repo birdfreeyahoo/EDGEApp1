b00080b4ea53ec2d84f702f59f61d453df090848
Dominik Mueller <mullerdome@yahoo.de>
2014-06-25 01:54:03 +0200

    Gameplay (Random Spawn, Speed, Objects) added

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,7 +1,112 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameController : MonoBehaviour 
 {
-    
+    public enum LevelStatus
+    {
+        Running,
+        Success,
+        Failed
+    }
+
+    protected float levelTimer = 0.0f;
+    protected float spawnTime = 0.0f;
+    protected float actualSpawnTime = 0.0f;
+
+    protected List<string> probabilityDictKeyList = new List<string>(GameInitialization.asteroidProbability.Keys); // Für Index-Zugriff auf Dictionary
+
+    protected List<GameObject> asteroids = new List<GameObject>();
+
+    protected virtual void Update()
+    {
+        TickTimer();
+
+        switch(GetLevelStatus())
+        {
+            case LevelStatus.Running:
+                break;
+            case LevelStatus.Success:
+                break;
+            case LevelStatus.Failed:
+                break;
+            default:
+                break;
+        }
+
+        TickSpawn();
+
+    }
+
+
+
+    protected virtual void TickSpawn()
+    {
+        if(levelTimer >= actualSpawnTime)
+        {
+            SpawnRandomAsteroid();
+
+            float intervalRandomRange = ((float)GameInitialization.intervalVariance / 100) * GameInitialization.spawnInterval; // Höhe der Abweichung
+            float minTime = GameInitialization.spawnInterval - intervalRandomRange; // Berechnet die Grenzen der Spawn-Ranges
+            float maxTime = GameInitialization.spawnInterval + intervalRandomRange;
+
+            float newInterval = Random.Range(minTime, maxTime);
+
+            actualSpawnTime = spawnTime + newInterval;
+            spawnTime += GameInitialization.spawnInterval;
+
+        }
+    }
+
+    protected virtual void SpawnRandomAsteroid()
+    {
+        int randomNumber = Random.Range(0, 100);
+        int probabilityCounter = 0;
+
+        string objectName = string.Empty;
+
+        for (int i = 0; i < GameInitialization.asteroidProbability.Count; i++) // Berechnet, welches Objekt genommen wird
+        {
+            int probability = GameInitialization.asteroidProbability[probabilityDictKeyList[i]];
+
+            if (randomNumber < (probability + probabilityCounter) && !(randomNumber < probabilityCounter))
+            {
+                objectName = probabilityDictKeyList[i];
+                break;
+            }
+
+            probabilityCounter += probability;
+        }
+
+        Object asteroidObject = Resources.Load(objectName);
+
+        float spawnX = Random.Range(GameInitialization.cameraRect.xMin, GameInitialization.cameraRect.xMax);
+        float spawnY = Random.Range(GameInitialization.cameraRect.yMin, GameInitialization.cameraRect.yMax);
+
+        Debug.Log(GameInitialization.cameraRect.yMin + " " + GameInitialization.cameraRect.yMax);
+
+        GameObject instantiated = (GameObject)Instantiate(asteroidObject, new Vector3(spawnX, spawnY, GameInitialization.spawnZValue), Quaternion.identity);
+
+        instantiated.rigidbody.AddForce((Vector3.back * (GetRandomSpeed() + GameInitialization.playerSpeed)));
+    }
+
+    protected virtual float GetRandomSpeed()
+    {
+        float speedRange = ((float)GameInitialization.asteroidSpeedVariance / 100) * GameInitialization.asteroidSpeed;
+        float speedMin = GameInitialization.asteroidSpeed - speedRange;
+        float speedMax = GameInitialization.asteroidSpeed + speedRange;
+
+        return Random.Range(speedMin, speedMax);
+    }
+
+    protected virtual void TickTimer()
+    {
+        levelTimer += Time.deltaTime;
+    }
+
+    protected virtual LevelStatus GetLevelStatus()
+    {
+        return LevelStatus.Running;
+    }
 }
--- Assets/Scripts/GameInitialization.cs
+++ Assets/Scripts/GameInitialization.cs
@@ -5,15 +5,24 @@ public static class GameInitialization
 {
     public static Texture backgroundTexture;
 
-    public static float playerSpeed = 2.0f;
+    public static float playerSpeed = 50.0f;
 
-    public static float asteroidSpeed = 2.0f;
+    public static float asteroidSpeed = 250.0f;
     public static int asteroidSpeedVariance = 10;
 
-    public static Dictionary<GameObject, int> asteroidProbability = new Dictionary<GameObject, int>();
+    public static Dictionary<string, int> asteroidProbability = new Dictionary<string, int>
+    {
+        { "asteroidSmall", 80 },
+        { "asteroidMedium", 10},
+        { "asteroidBig", 10}
+    };
 
     public static float spawnInterval = 1.5f;
     public static int intervalVariance = 20;
+
+    public static Rect cameraRect = new Rect(-1.0f, -1.0f, 2.0f, 2.0f);
+
+    public static float spawnZValue = 20.0f;
 
     public static float levelTrack = 1000.0f;
 
