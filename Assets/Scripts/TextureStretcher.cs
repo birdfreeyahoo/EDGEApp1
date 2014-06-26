c434110223fc832a0d0de09bf66c303b6d8ee19f
Dominik Mueller <mullerdome@yahoo.de>
2014-06-27 00:33:39 +0200

    Texture Stretcher changed

--- Assets/Scripts/TextureStretcher.cs
+++ Assets/Scripts/TextureStretcher.cs
@@ -6,9 +6,9 @@ public class TextureStretcher : MonoBehaviour
     void Start()
     {
         Rect pixelInset = guiTexture.pixelInset;
-        pixelInset.width = Screen.width;
+        pixelInset.width = Screen.height * (16.0f / 9.0f);
         pixelInset.height = Screen.height;
-		pixelInset.x = -Screen.width/2;
+		pixelInset.x = -pixelInset.width/2;
 		pixelInset.y = -Screen.height/2;
 
         guiTexture.pixelInset = pixelInset;
